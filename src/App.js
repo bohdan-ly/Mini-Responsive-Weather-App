13ec107cbb3c82cbe17abb8a30762140a1a69b49
TwelveGEater <dronkfc@gmail.com>
2020-05-01 01:11:40 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 const api = {
@@ -8,12 +10,11 @@ const api = {
 
 function App() {
 	//const apiUrl = `api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_KEY}`;
-
 	const [ query, setQuery ] = useState('');
 	const [ weather, setWeather ] = useState('');
 
 	const Search = async (evt) => {
-		if (evt.key === 'Enter' || evt.type === 'blur') {
+		if (evt.key === 'Enter' || evt.type === 'click') {
 			let response = await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`);
 			let result = await response.json();
 			setWeather(result);
@@ -43,8 +44,11 @@ function App() {
 		let month = months[d.getMonth()];
 		let year = d.getFullYear();
 
-		return ` ${day} ${date} ${month} ${year}`;
+		return `${day} ${date} ${month} ${year}`;
 	};
+
+	let time = new Date().getHours();
+
 	return (
 		<div className="app-wrapper">
 			<div className="search-box">
@@ -54,11 +58,36 @@ function App() {
 					placeholder="Search..."
 					onChange={(e) => setQuery(e.target.value)}
 					value={query}
-					onBlur={Search}
 					onKeyPress={Search}
 				/>
-			</div>
-			<div className={(!!weather.main && (weather.main.temp >= 16 ? 'warm' : 'app')) || 'app'}>
+				<div className="search-button">
+					<button onClick={Search}>
+						<FontAwesomeIcon icon={faSearch} />
+					</button>
+				</div>
+			</div>{' '}
+			<div
+				className={
+					weather.main === undefined ? time >= 22 ? (
+						'night'
+					) : time > 18 ? (
+						'evening'
+					) : time > 12 ? (
+						'sunshine'
+					) : time > 8 ? (
+						'morning'
+					) : time > 5 ? (
+						'dawn'
+					) : (
+						'night'
+					) : weather.main.temp > 16 ? (
+						'warm'
+					) : (
+						'app'
+					)
+				}
+			>
+				{/* 			(!!weather.main && (weather.main.temp >= 16 ? 'warm' : 'app')) || 'app'	*/}
 				<main>
 					{!!weather.main && (
 						<div className="location-box">
@@ -69,6 +98,7 @@ function App() {
 							<div className="weather-box">
 								<div className="temp">{Math.round(weather.main.temp)}°C</div>
 								<div className="weather">{weather.weather[0].main}</div>
+								<div className="wind__speed">Wind speed: {Math.round(weather.wind.speed)}km/h </div>
 							</div>
 						</div>
 					)}
